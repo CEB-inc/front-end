1b9bb63cfdf84795cac24143a54784858ae3deb4
Ethan <ethanjneyland@gmail.com>
2022-07-15 16:14:16 +1000

    functionless edit and delete button added

--- src/components/pages/ShowEntry.jsx
+++ src/components/pages/ShowEntry.jsx
@@ -1,8 +1,14 @@
 function ShowEntry({ entry }) {
   return entry ? (
     <>
+    <div className='px-3 py-2'>
       <h5>{entry.entry}</h5>
       <p>Posted in {entry.category}</p>
+      <div classType='btn-toolbar'>
+          <button id='editbutt' className='btn px-4 fw-semibold'>Edit</button>
+          <button id='dltbutt' className='btn px-4 btn-danger fw-semibold'>Delete</button>
+      </div>    
+    </div>
     </>
   ) : (
     <p>Loading ...</p>

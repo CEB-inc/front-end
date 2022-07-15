e7fe4d1e5885847240bcb3f8941a281027ad7ad3
Ethan <ethanjneyland@gmail.com>
2022-07-15 13:50:18 +1000

    styling to create post form and navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,27 @@
 import { Link } from 'react-router-dom'
+import '../index.css'
 
 function Nav() {
 	return (
-	<nav className="navbar navbar-expand-lg bg-light">
-		<div className="container-fluid">
-			<a className="navbar-brand" href="#">Media Hub</a>
-			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon"></span>
-			</button>
-			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-				<div className="navbar-nav">
-					<Link className="nav-link active" aria-current="page" to="/">Home</Link>
-					<Link className="nav-link" to="/category">Create Post</Link>
+	<>	
+		<nav id='navbar' className="navbar navbar-expand-sm d-flex align-items-center mb-2">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="/">Media Hub</a>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div className="navbar-nav me-auto">
+						<Link className="nav-link" to="/category">Create Post +</Link>
+					</div>
+					<div className="navbar-nav">
+						<Link className="nav-link active fw-semibold" to="">Login</Link>
+						<Link className="nav-link active fw-semibold" to="">Sign Up</Link>
+					</div>
 				</div>
 			</div>
-		</div>
-	</nav>
+		</nav>
+	</>	
 	)
 }
 
--- src/components/pages/NewEntry.jsx
+++ src/components/pages/NewEntry.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import '/src/index.css'
 
 function NewEntry({ addEntry }) {
   
@@ -15,12 +16,15 @@ function NewEntry({ addEntry }) {
 
   return (
     <>
-    <h2>New Entry for {category} </h2>
-    <form className='container-sm' onSubmit={submit}>
-      <div>
-        <textarea value={entry} onChange={e => setEntry(e.target.value)} rows='10' className='form-control'></textarea>
+    <div className='ms-3'>
+      <h3>New Entry for {category} </h3>
+      <p>Select a post type: <strong>Blog</strong> or <strong>Review</strong></p>
+    </div>
+    <form className='container-sm float-start px-3' onSubmit={submit}>
+      <div className=''>
+        <textarea className='form-control mb-3' value={entry} onChange={e => setEntry(e.target.value)} rows='10' ></textarea>
       </div>
-      <button className='btn btn-primary mt-3'>Post</button>
+      <button id='postbutt' className='btn px-5 fw-semibold'>Post</button>
     </form>
     </>
   )
